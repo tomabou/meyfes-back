c8776fd7e67c6191b973cb5f0643da087100bc59
tomabou <kazu.8128.9.6@gmail.com>
2019-05-04 21:53:01 +0900

    SAVE converted file

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from lib.transform import transform_main, show_img
 from lib.server import get_image_file, create_raw_file
 import numpy as np
 import cv2
+import os
+import random
 
 from flask import Flask, request, send_file, jsonify
 from flask_cors import CORS
@@ -10,17 +12,28 @@ app = Flask(__name__)
 CORS(app)
 
 
+def get_file_path(image_id):
+    return "./image/"+str(image_id)+".png"
+
+
 @app.route("/", methods=['POST'])
 def post():
     img = get_image_file(request)
     img = transform_main(img)
-    data = create_raw_file(img)
-    return jsonify({'image_url': 'http://localhost:5000'})
+    image_id = random.randint(1, 1e20)
+    cv2.imwrite(get_file_path(image_id), img)
+
+    return jsonify({'image_url': 'http://localhost:5000?id={}'.format(image_id)})
 
 
 @app.route("/", methods=['GET'])
 def get():
-    return send_file("./image/test.jpg", mimetype='image/jpeg')
+    image_id = request.args.get("id", default=0, type=int)
+    file_path = get_file_path(image_id)
+    if not os.path.exists(file_path):
+        file_path = "./image/test.jpg"
+
+    return send_file(file_path, )
 
 
 def main():
